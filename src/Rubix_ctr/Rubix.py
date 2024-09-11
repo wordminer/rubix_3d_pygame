3162544ca09acf9d5372e6e8f76e1586c6ec5ddc
wordminer <adamevama@gmail.com>
2024-09-11 20:56:48 +0700

    fix

--- src/Rubix_ctr/Rubix.py
+++ src/Rubix_ctr/Rubix.py
@@ -13,7 +13,7 @@ class Rubix_cube():
         Start_point = [-(self.block_side + self.block_distance)*(block-1)/2 
                        for block in [self.block_x, self.block_y, self.block_z]]
         
-        block = [
+        self.block = [
             [Start_point[k] + (self.block_side + self.block_distance) * Pos for k, Pos in enumerate([x,y,z]) ] 
 
             for x in range(self.block_x) 
